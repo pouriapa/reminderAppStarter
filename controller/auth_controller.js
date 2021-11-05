77aecb2f4d8b79bcd650383c12e1283200b7c14d
unknown <pouria.pourarjmand@gmail.com>
2021-11-05 16:02:57 -0700

    upload reminder update and delete handlers

---
+++ controller/auth_controller.js
@@ -0,0 +1,23 @@
+let database = require("../database");
+
+let authController = {
+  login: (req, res) => {
+    res.render("auth/login");
+  },
+
+  register: (req, res) => {
+    res.render("auth/register");
+  },
+
+  loginSubmit: (req, res) => {
+    // implement
+  },
+
+  registerSubmit: (req, res) => {
+    // implement
+    console.log(req.body);
+    
+  },
+};
+
+module.exports = authController;
---
+++ database.js
@@ -0,0 +1,10 @@
+let Database = {
+    cindy: {
+        reminders: [{id: 1, title: "first reminder", description: "first description for Cindy", completed: false}]
+    },
+    alex: {
+        reminders: []
+    } 
+}
+
+module.exports = Database;
